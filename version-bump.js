b975280187ad647b820ebd6dda0c3b6230f6d257
neg1t <n.fedchenko@solutionfactory.ru>
2023-08-17 01:16:30 +0700

    feat: 123

--- version-bump.js
+++ version-bump.js
@@ -21,51 +21,50 @@ const commitTypes = {
 const options = {
   repo: __dirname,
   number: 100, // максимальное количество коммитов для анализа
-  fields: ["hash", "subject", "files"],
-  since: `${currentVersion}`,
+  fields: ["hash", "subject"],
 };
 
 const commits = gitlog(options);
 
-// Проверяем, были ли изменения в package.json
-const hasPackageJsonChanges = commits.some((commit) => {
-  return commit.files.includes("package.json");
-});
+// Считаем количество коммитов с разными типами изменений
+const commitCountByType = {};
 
-if (hasPackageJsonChanges) {
-  // Определение, какой тип версии увеличивать
-  let versionType = "patch"; // По умолчанию, увеличиваем патч-версию
-
-  for (const commit of commits) {
-    const commitType = commit.subject.split(":")[0].trim();
-    if (commitTypes[commitType]) {
-      versionType = commitTypes[commitType];
-      break; // Берем первый подходящий тип и выходим из цикла
-    }
+for (const commit of commits) {
+  const commitType = commit.subject.split(":")[0].trim();
+  if (commitTypes[commitType]) {
+    commitCountByType[commitTypes[commitType]] =
+      (commitCountByType[commitTypes[commitType]] || 0) + 1;
   }
-
-  // Увеличение версии в соответствии с определенным типом
-  const versionParts = currentVersion.split(".");
-  versionParts[versionType === "patch" ? 2 : 1] = (
-    parseInt(versionParts[versionType === "patch" ? 2 : 1], 10) + 1
-  ).toString();
-  const newVersion = versionParts.join(".");
-
-  // Обновление версии в package.json
-  packageJson.version = newVersion;
-  jsonfile.writeFileSync(packageJsonPath, packageJson, { spaces: 2 });
-
-  console.log(`Версия увеличена с ${currentVersion} до ${newVersion}`);
-
-  // Создание коммита с обновленной версией
-  const commitCommand = `git commit -am "new version ${newVersion}"`;
-  execSync(commitCommand);
-
-  // Произведение git push
-  const pushCommand = "git push";
-  execSync(pushCommand);
-
-  console.log("Коммит и push выполнены");
-} else {
-  console.log("Изменений в package.json не обнаружено. Коммит не требуется.");
 }
+
+// Определение, какой тип версии увеличивать
+let versionType = "patch"; // По умолчанию, увеличиваем патч-версию
+
+if (commitCountByType.minor > 0) {
+  versionType = "minor"; // Если есть коммиты с новыми функциями, увеличиваем минор-версию
+} else if (commitCountByType.patch > 0) {
+  versionType = "patch"; // В противном случае, увеличиваем патч-версию
+}
+
+// Увеличение версии в соответствии с определенным типом
+const versionParts = currentVersion.split(".");
+versionParts[versionType === "patch" ? 2 : 1] = (
+  parseInt(versionParts[versionType === "patch" ? 2 : 1], 10) + 1
+).toString();
+const newVersion = versionParts.join(".");
+
+// Обновление версии в package.json
+packageJson.version = newVersion;
+jsonfile.writeFileSync(packageJsonPath, packageJson, { spaces: 2 });
+
+console.log(`Версия увеличена с ${currentVersion} до ${newVersion}`);
+
+// Создание коммита с обновленной версией
+const commitCommand = `git commit -am "new version ${newVersion}"`;
+execSync(commitCommand);
+
+// Произведение git push
+const pushCommand = "git push";
+execSync(pushCommand);
+
+console.log("Коммит и push выполнены");
