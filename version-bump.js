eaf80df0365923b43244ec7a78cd686b55e0eb3e
neg1t <n.fedchenko@solutionfactory.ru>
2023-08-17 02:28:01 +0700

    feat: script updated

--- version-bump.js
+++ version-bump.js
@@ -1,6 +1,5 @@
-// const jsonfile = require("jsonfile");
-// const path = require("path");
-// const execSync = require("child_process").execSync;
+const jsonfile = require("jsonfile");
+const path = require("path");
 const gitlog = require("gitlog").default;
 
 // const packageJsonPath = path.join(__dirname, "package.json");
@@ -72,13 +71,32 @@ const gitlog = require("gitlog").default;
 // const pushCommand = "git push";
 const { execSync } = require("child_process");
 
-// execSync(`git status`);
+const KEY_WORD = "new version";
+
+function updateVersion(type, version) {
+  const types = {
+    fix: 2,
+    feat: 1,
+    update: 0,
+  };
+  return version
+    .split(".")
+    .map((item, index) => (index === types[type] ? +item + 1 : item))
+    .join(".");
+}
+
+const packageJsonPath = path.join(__dirname, "package.json");
+
+// Загрузка текущей версии из package.json
+const packageJson = jsonfile.readFileSync(packageJsonPath);
+const currentVersion = packageJson.version;
+let newVersion = currentVersion;
 
 // Определение типов изменений и соответствующих ключевых слов
 const commitTypes = {
   fix: "patch", // Исправления - увеличиваем патч-версию
   feat: "minor", // Новые функции - увеличиваем минор-версию
-  update: "major", // Изменения без разрыва - увеличиваем минор-версию
+  update: "major", // Новая версия - увеличиваем минор-версию
   // Другие типы изменений, если необходимо
 };
 
@@ -90,18 +108,31 @@ const options = {
 const commits = gitlog(options);
 
 for (const commit of commits) {
-  const isNewVersion = commit.subject.includes("new version");
+  const isNewVersion = commit.subject.includes(KEY_WORD);
   if (isNewVersion) {
     console.log("Найден коммит с новой версией");
-    // execSync(`git push`);
-    // console.log("Push выполнен");
     return;
+  }
+  const commitType = commit.subject.split(":")[0].trim();
+  if (commitTypes[commitType]) {
+    newVersion = updateVersion(commitType, newVersion);
   }
   console.log(commit);
 }
 
-// console.log(commits);
+// Если версия изменилась, то обновляем ее в package.json и создаем коммит
+if (newVersion !== currentVersion) {
+  // Обновление версии в package.json
+  packageJson.version = newVersion;
+  jsonfile.writeFileSync(packageJsonPath, packageJson, { spaces: 2 });
 
-// execSync(`git push`);
+  // Создание коммита с обновленной версией
+  const commitCommand = `git commit -am "${KEY_WORD} ${newVersion}"`;
+  execSync(commitCommand);
+
+  console.log(`Версия увеличена с ${currentVersion} до ${newVersion}`);
+} else {
+  console.log("Версия не изменилась");
+}
 
 console.log("Коммит и push выполнены");
