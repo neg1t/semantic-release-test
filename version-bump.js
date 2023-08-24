70a73bce624c0b37a5a0cb568ba47c44c6f9f7ab
neg1t <n.fedchenko@solutionfactory.ru>
2023-08-24 23:05:54 +0700

    fix: asd

--- version-bump.js
+++ version-bump.js
@@ -68,7 +68,7 @@ for (const commit of parsedCommits) {
 
 // Если версия изменилась, то обновляем ее в package.json и создаем коммит
 if (newVersion !== currentVersion) {
-  // Обновление версии в package.json asdasd
+  // Обновление версии в package.json asdasdasd
   packageJson.version = newVersion;
   jsonfile.writeFileSync(packageJsonPath, packageJson, { spaces: 2 });
 
