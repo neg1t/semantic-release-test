58b3231a161806b16daf43dd846ff526f96297f5
neg1t <n.fedchenko@solutionfactory.ru>
2023-08-25 00:02:24 +0700

    asd

--- version-bump.js
+++ version-bump.js
@@ -95,7 +95,7 @@ if (newVersion !== currentVersion) {
   // execSync(`git rebase origin/master`);
   // Произведение git push
   const pushCommand = "git push --no-verify";
-  // execSync(pushCommand);
+  execSync(pushCommand);
 
   console.log(`Версия увеличена с ${currentVersion} до ${newVersion}`);
   console.log("Коммит и push выполнены");
